576ab370b48b47c464bb2869af452d2a934ce798
Nikhil Vanamala <105160327+Nickhil215@users.noreply.github.com>
2023-04-15 22:05:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
 
+ let fname=document.getElementsByName('fname');
+    let lname=document.getElementsByName('lname');
+    alert(fname+" "+ lname);
+	
 }
